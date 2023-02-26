86877ac35ef34f0b59a23bb79faf1254ed69cf9d
Vishnyakov-Developer <98761941+Vishnyakov-Developer@users.noreply.github.com>
2023-02-26 17:50:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -233,7 +233,7 @@ function openPage(page) {
 }
 
 function windowCatalog(catalogid, platform) {
-    const URL = 'http://124699124.online/catalog?';
+    const URL = 'https://124699124.online/catalog?';
     window.location.replace(URL + `from=${0}&limit=70&catalogid=${catalogid}&search=`);
 }
 
